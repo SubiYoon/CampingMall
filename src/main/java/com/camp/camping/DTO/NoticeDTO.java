224ae62687a902ab912b9f4da3a3f64e2e25bede
KMuSeung <peter0757@nvaer.com>
2023-01-09 17:14:07 +0900

    NoticeDTO 추가

---
+++ src/main/java/com/camp/camping/DTO/NoticeDTO.java
@@ -0,0 +1,20 @@
+package com.camp.camping.DTO;
+
+import lombok.*;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class NoticeDTO {
+    private int notice_code;
+    private int notice_level;
+    private String notice_title;
+    private String notice_writer;
+    private Date notice_writedate;
+    private String notice_content;
+
+}
