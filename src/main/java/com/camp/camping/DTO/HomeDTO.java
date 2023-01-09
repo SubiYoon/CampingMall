045939c0952b1dd8069cb02dcbd439426f7815f9
KMuSeung <peter0757@nvaer.com>
2023-01-09 17:18:56 +0900

    HomeDTO 추가

---
+++ src/main/java/com/camp/camping/DTO/HomeDTO.java
@@ -0,0 +1,16 @@
+package com.camp.camping.DTO;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class HomeDTO {
+    private int home_code;
+    private String home_content;
+    private String home_img1;
+    private String home_img2;
+    private String home_img3;
+}
