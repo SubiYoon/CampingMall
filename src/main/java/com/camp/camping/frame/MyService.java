26f0d53a01c0f716e88b368634e1e1d6985b792c
YoonDongSub <yoondongsub92@gmail.com>
2023-01-10 13:48:00 +0900

    frame edit

--- src/main/java/com/camp/camping/frame/MyService.java
+++ src/main/java/com/camp/camping/frame/MyService.java
@@ -3,10 +3,10 @@ package com.camp.camping.frame;
 import java.util.List;
 
 public interface MyService<K,V> {
-    public void register(V v) throws Exception;
-    public void remove(K k) throws Exception;
-    public void modify(V v) throws Exception;
+    public void insert(V v) throws Exception;
+    public void delete(K k) throws Exception;
+    public void update(V v) throws Exception;
 
-    public V get(K k) throws Exception;
-    public List<V> get() throws Exception;
+    public V select(K k) throws Exception;
+    public List<V> selectAll() throws Exception;
 }
