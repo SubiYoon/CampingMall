e6a9da525e9149b4dbee2ba5b7ee69f1bf42a0e4
taewook2 <mukno789@gmail.com>
2023-01-12 17:28:00 +0900

    import test2

--- src/main/java/com/camp/camping/DTO/PaymentDTO.java
+++ src/main/java/com/camp/camping/DTO/PaymentDTO.java
@@ -1,6 +1,5 @@
 package com.camp.camping.DTO;
 
-<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,19 +11,7 @@ public class PaymentDTO {
 	
 	private String imp_uid;
 	private String merchant_uid;
+	private String pay_method;
 	
 
-=======
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString
-public class PaymentDTO {
-    private int payment_code;
-    private String payment_type;
-    private int payment_uid;
->>>>>>> a7783e95bd5427b35a647b8c979cce0094b6f3e4
 }
