54803c7aa3de1d6a19cfd894a6fefc8febff92eb
YoonDongSub <yoondongsub92@gmail.com>
2023-01-14 21:27:40 +0900

    Yoon UserInsertTest

--- src/main/java/com/camp/camping/Controller/UserController.java
+++ src/main/java/com/camp/camping/Controller/UserController.java
@@ -1,5 +1,7 @@
 package com.camp.camping.Controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,32 +13,52 @@ import com.camp.camping.service.UserService;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-    
-    String dir = "user/";
 
-    @Autowired
-    UserService service;
+	String dir = "user/";
 
-    @RequestMapping("register")
-    public String register(Model model){
-        model.addAttribute("center", dir+"register");
-        return "main";
-    }
+	@Autowired
+	UserService service;
 
-    @RequestMapping("registerOk")
-	public String registerOk(Model model, UserDTO userDTO){
-		
-		String user_phone_number = userDTO.getUser_tel1() + "-" + userDTO.getUser_tel2() + "-" +userDTO.getUser_tel3();
+	@RequestMapping("register")
+	public String register(Model model) {
+		model.addAttribute("center", dir + "register");
+		return "main";
+	}
+
+	@RequestMapping("registerOk")
+	public String registerOk(Model model, UserDTO userDTO) {
+
+		String user_phone_number = userDTO.getUser_tel1() + "-" + userDTO.getUser_tel2() + "-" + userDTO.getUser_tel3();
 
 		userDTO.setUser_phone_number(user_phone_number);
 
 		try {
-			//service.insert(userDTO);
+			service.insert(userDTO);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		System.out.println(userDTO.toString());
+		return "main";
+	}
+
+	@RequestMapping("mypage")
+	public String myPage(Model model, UserDTO userDTO, HttpSession session) {
+
+		UserDTO user = null;
+
+		try {
+			user = service.select(userDTO.getUser_code());
+			
+			String[] telNum = user.getUser_phone_number().split("-");
+			user.setUser_tel1(telNum[0]);
+			user.setUser_tel2(telNum[1]);
+			user.setUser_tel3(telNum[2]);
+
+			model.addAttribute("selectUser", user);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		return "main";
 	}
---
+++ src/test/java/com/camp/camping/User/UserInsertText.java
@@ -0,0 +1,31 @@
+package com.camp.camping.User;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.camp.camping.DTO.UserDTO;
+import com.camp.camping.service.UserService;
+
+@SpringBootTest
+class UserInsertText {
+
+	@Inject
+	UserService service;
+
+	@Test
+	void contextLoads() {
+
+		UserDTO user = new UserDTO(0, "user011", "pwd001", "우하하", 123456, "010-1234-4567", "010", "1234", "5678");
+
+		try {
+			service.insert(user);
+			System.out.println("성공123");
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("실패");
+		}
+	}
+
+}
