a9de234615a3dfc65d8984e85f5ca059ab5a0618
BHGoo <gbh1055@naver.com>
2023-01-16 15:01:21 +0900

    notice 일부 작업

--- src/main/java/com/camp/camping/Controller/NoticeController.java
+++ src/main/java/com/camp/camping/Controller/NoticeController.java
@@ -1,8 +1,14 @@
 package com.camp.camping.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.camp.camping.DTO.NoticeDTO;
+import com.camp.camping.service.NoticeService;
 
 @Controller
 @RequestMapping("/notice")
@@ -10,8 +16,18 @@ public class NoticeController {
 	
 	String dir = "notice/";
 	
+	@Autowired
+	NoticeService ns;
+	
 	@RequestMapping("")
 	public String main(Model model) {
+		List<NoticeDTO> list = null;
+		try {
+			list = ns.selectAll();
+			model.addAttribute("plist", list);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		model.addAttribute("center", dir+"notice");
 		return "main";
 	}
