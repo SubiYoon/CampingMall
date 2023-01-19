b3c558cf253583371c4a41d9238efb3c76f4ec57
JiwonPark <king.jiwon.park@gmail.com>
2023-01-19 15:05:25 +0900

    ParkJW 카카오맵이 home_code=1(admin_code=1)일때의 맵을 보여주도록 구성

--- src/main/java/com/camp/camping/Controller/MainController.java
+++ src/main/java/com/camp/camping/Controller/MainController.java
@@ -1,14 +1,32 @@
 package com.camp.camping.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.camp.camping.DTO.HomeDTO;
+import com.camp.camping.service.HomeService;
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	HomeService service;
 	
 	@RequestMapping("/")
-	public String main() {
+	public String main(Model model) {
+		
+		HomeDTO home = null;
+		
+		try {
+			home = service.lnglat(2);	//홈페이지코드
+			model.addAttribute("kakaomap", home);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("FAIL");
+		}
+		
 		return "main";
 	}
 	
