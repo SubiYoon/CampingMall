959899b851c121fa2e8024be2e56154389cedb71
taewook2 <mukno789@gmail.com>
2023-01-31 01:30:30 +0900

    bookdetail

--- src/main/java/com/camp/camping/Controller/BookController.java
+++ src/main/java/com/camp/camping/Controller/BookController.java
@@ -1,10 +1,18 @@
 package com.camp.camping.Controller;
 
+import com.camp.camping.DTO.BookDTO;
 import com.camp.camping.service.BookService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/book")
@@ -19,5 +27,31 @@ public class BookController {
 		model.addAttribute("center", dir+"book");
 		return "main";
 	}
+	
+	
+	@RequestMapping("/bookdetail")
+	public String book_detail(Model model,
+						      @RequestParam("book_checkin")String book_checkin,
+						      @RequestParam("book_checkout")String book_checkout,
+						      @RequestParam("book_sitecode")String book_sitecode) {
+		model.addAttribute("center", dir+"bookdetail");
+		
+		
+		
+		BookDTO book=new BookDTO();
+		
+		DateFormat dateFormat = new SimpleDateFormat("MMddHHmmss");
+		Calendar cal = Calendar.getInstance();
+		Random random = new Random();
+		book.setMerchant_uid(dateFormat.format(cal.getTime())+""+random.nextInt(100000));
+		book.setSite_code(Integer.parseInt(book_sitecode));
+		
+		return "main";
+	}
+	
+	
+	
+	
+	
 	
 }
