2663aec5d579cbbbe92666a9747d1c1475589b12
YoonDongSub <yoondongsub92@gmail.com>
2023-01-31 16:20:54 +0900

    Yoon 시설완료

--- src/main/java/com/camp/camping/DTO/FacilityDTO.java
+++ src/main/java/com/camp/camping/DTO/FacilityDTO.java
@@ -12,4 +12,14 @@ public class FacilityDTO {
     private int admin_code;
     private String facility_name;
     private String facility_content;
+
+    private int image_code;
+    private String image_file;
+
+    public FacilityDTO(int facility_code, int admin_code, String facility_name, String facility_content){
+        this.facility_code = facility_code;
+        this.admin_code = admin_code;
+        this.facility_name = facility_name;
+        this.facility_content = facility_content;
+    }
 }
