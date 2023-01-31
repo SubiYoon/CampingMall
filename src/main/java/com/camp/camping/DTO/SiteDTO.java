ee7ff6995d90945d6e73e0b60dc0113e34fabe74
YoonDongSub <yoondongsub92@gmail.com>
2023-01-31 17:25:11 +0900

    Yoon 시설, 홈, 사이트 완료

--- src/main/java/com/camp/camping/DTO/SiteDTO.java
+++ src/main/java/com/camp/camping/DTO/SiteDTO.java
@@ -1,6 +1,10 @@
 package com.camp.camping.DTO;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +17,15 @@ public class SiteDTO {
     private String site_name;
     private int site_price;
     private String site_content;
+
+    private int image_code;
+    private String image_file;
+
+    public SiteDTO(int site_code, int zone_code, String site_name, int site_price, String site_content) {
+        this.site_code = site_code;
+        this.zone_code = zone_code;
+        this.site_name = site_name;
+        this.site_price = site_price;
+        this.site_content = site_content;
+    }
 }
