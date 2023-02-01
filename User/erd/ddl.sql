1e86dd5744f22102e1c14a00edef8e7d453cca13
KMuSeung <peter0757@nvaer.com>
2023-02-01 17:54:01 +0900

    무성 / ddl  수정(차량번호를 주문상세로 활용 / TODO: 추후 칼럼명을 book_details로 변경예정)

--- User/erd/ddl.sql
+++ User/erd/ddl.sql
@@ -68,6 +68,7 @@ CREATE TABLE `book` (
    `book_price`   INT   NOT NULL,
    `book_car_number`   VARCHAR(8)   NULL,
    `book_state` INT NULL DEFAULT 1,
+   `book_detials` VARCHAR(500) NULL,
     primary key (book_code)
 );
 
