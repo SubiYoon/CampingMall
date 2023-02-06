79cc3b7a5c87cfced5aeeae2e3825cf7adfc4cbb
MuSeung <peter0757@naver.com>
2023-02-06 23:13:27 +0900

    무성 / 임시 인덱스

--- admin/src/main/java/com/camp/admin/Controller/AjaxController.java
+++ admin/src/main/java/com/camp/admin/Controller/AjaxController.java
@@ -1,8 +1,30 @@
 package com.camp.admin.Controller;
 
+import com.camp.admin.DTO.CompanyDTO;
+import com.camp.admin.DTO.GraphDTO;
+import com.camp.admin.service.BookService;
+import javax.servlet.http.HttpSession;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AjaxController {
-    
+    @Autowired
+    BookService bService;
+
+    @RequestMapping("main/graphData")
+    public Object reviewUpload(HttpSession session, String stringDate){
+        JSONObject json = new JSONObject();
+        CompanyDTO company = (CompanyDTO)session.getAttribute("company");
+        try {
+            GraphDTO graphDTO = bService.MonthlyZoneSalesGraph("2023-02",company.getCompany_code());
+            json.put("Graph", graphDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
 }
